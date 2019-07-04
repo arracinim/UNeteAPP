5aa562625f5c94f657587d52338a4eb27ccf1262
David18236 <39032388+David18236@users.noreply.github.com>
2019-07-04 14:57:56 -0500

    Se agrega Modificar Perfil

---
+++ app/Http/Controllers/ModificarPerfilController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ModificarPerfilController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit()
+    {
+        $user = User::find(auth()->user()->id);
+        return view('users.editPerfil',compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $user = User::find(auth()->user()->id);
+        $user->update($request->all());
+
+        return redirect() -> route('home')
+            -> with('success','Perfil actualizado exitosamente');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
